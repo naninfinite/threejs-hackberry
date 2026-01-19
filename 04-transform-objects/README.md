49878283dec17946a4d7653786bb760b83a2ce34
naninfinite <215418892+naninfinite@users.noreply.github.com>
2026-01-19 14:35:25 +0000

    trying out object transforms

---
+++ 04-transform-objects/README.md
@@ -0,0 +1,9 @@
+issues
+
+ran into issues with material not rendering correctly. first i rushed and put geometry coordinates into the mesh basic material
+
+after changing i had an issue with 2 objects centre and middle not displaying red material they were still white
+
+commented out console logs and error fixed
+
+straight forward exercise
---
+++ 04-transform-objects/src/script.js
@@ -0,0 +1,87 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
+/**
+ * Objects
+ */
+//const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+//const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//const mesh = new THREE.Mesh(geometry, material)
+//scene.add(mesh)
+//mesh.position.x = 0.7
+//mesh.position.y = -0.6
+//mesh.position.z = 1
+//mesh.position.set(0.7, -0.6, 1)
+//mesh.scale.set(2, 0.25, 0.5)
+//mesh.rotation.x = Math.PI * 0.25
+//mesh.rotation.y = Math.PI * 0.25
+
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000})
+)
+cube1.position.x = -1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+//camera.lookAt(new THREE.Vector3(0, -1, 0))
+//camera.lookAt(camera.position)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+/*
+console.log(mesh.position.length)
+console.log(mesh.position.distanceTo(camera.position))
+console.log(mesh.position.normalize())
+*/
