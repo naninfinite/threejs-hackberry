e5fbeb78fddca6c7028632841ae25fd46117878b
naninfinite <215418892+naninfinite@users.noreply.github.com>
2026-01-19 16:06:51 +0000

    tick rotation

---
+++ 05-animations/src/script.js
@@ -0,0 +1,51 @@
+import * as THREE from 'three'
+import { thickness } from 'three/tsl'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+/* 
+Animate
+*/
+const tick = () => {
+
+//console.log('tick')
+
+//update objects
+mesh.rotation.y += 0.01
+
+//render
+renderer.render(scene, camera)
+
+//call tick again on the next frame
+window.requestAnimationFrame(tick)
+
+
+}
+tick()
